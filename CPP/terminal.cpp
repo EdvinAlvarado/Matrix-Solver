73e3d179db65b0c41817cc7d9be3aa8d37264bb3
EdvinAlvarado <ealvarado301@gmail.com>
2019-04-02 17:01:21 -0400

    up

--- CPP/terminal.cpp
+++ CPP/terminal.cpp
@@ -1,11 +1,20 @@
 #include <iostream>
 #include "terminal_command.h"
 #include <string>
+#include <vector>
 
 using namespace std;
-void command_lookup(char arg) {
+vector<double> m;
+int rowsize;
+int colsize;
+void command_lookup(char arg) { //Maybe change char to string
 	if (arg == "exit") {
 		shell_exit()
+	}
+	elif (arg.find("matrix") == 0) {
+		string data_file_name = arg.substr(6,arg.size()-7);
+		m = shell_matrix(data_file_name);
+		x2+x-s=0
 	}
 }
 
