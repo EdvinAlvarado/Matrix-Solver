6d5ac3d53906442ef85deffc7a4fcbf139749428
EdvinAlvarado <ealvarado301@gmail.com>
2019-04-03 09:06:07 -0400

    establishing command lookup and setting the skeleton of terminal command

--- CPP/terminal.cpp
+++ CPP/terminal.cpp
@@ -2,20 +2,41 @@
 #include "terminal_command.h"
 #include <string>
 #include <vector>
+#include <cmath> // sqrt
 
 using namespace std;
-vector<double> m;
-int rowsize;
-int colsize;
+vector<double> m; // matrix
+int n; // dimension length of the main matrix
 void command_lookup(char arg) { //Maybe change char to string
 	if (arg == "exit") {
-		shell_exit()
+		shell_exit();
 	}
-	elif (arg.find("matrix") == 0) {
+	else if (arg.find("matrix") == 0) {
 		string data_file_name = arg.substr(6,arg.size()-7);
 		m = shell_matrix(data_file_name);
-		x2+x-s=0
+		n = (-1+sqrt(1+4*m.size()))/2;
+		shell_view(m);
+		MatrixChecker(m);
 	}
+	else if (arg == "view") {
+		shell_view(m);
+		MatrixChecker(m);
+	}
+	else if (arg.find("rowop") == 0) {
+		// add rowop function and and arg extractor
+		shell_view(m);
+		MatrixChecker(m);
+
+	}
+	else if (arg.find("rowmult") == 0) {
+		// add rowop function and and arg extractor
+		shell_view(m);
+		MatrixChecker(m);
+	}
+	else {
+		cout << "Unknown Command" << endl;
+	}
+	cout << "\n";
 }
 
 
--- CPP/terminal_command.h
+++ CPP/terminal_command.h
@@ -1,5 +1,21 @@
 #include <string>
 
-int shell_exit() {
+using namespace std;
+
+void shell_exit() {
 	return 0;
 }
+
+auto shell_matrix() {
+	return 0;
+}
+
+void shell_view(auto matrix) {
+	cout << "matrix placeholder" << endl;
+}
+
+int MatrixChecker(auto matrix) {
+	cout << "matrix checker" << endl;
+}
+
+void rowop(auto matrix, char arg, int rowsize)
